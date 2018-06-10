ca60a28daed4bd8aad9cf6da46a5d787edfd6cb7
sheinbey <yael>
2018-06-10 18:07:57 +0300

    start client

---
+++ client/app.js
@@ -0,0 +1,21 @@
+let app = angular.module('citiesApp', ["ngRoute"]);
+
+app.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider)  {
+
+
+    $locationProvider.hashPrefix('');
+
+
+    $routeProvider.when('/', {
+        template: '<h1>This is the default route</h1>'
+    })
+        .when('/about', {
+            templateUrl: 'components/About/about.html',
+            controller : 'aboutController as abtCtrl'
+        })
+        .when('/poi', {
+            templateUrl: 'components/POI/poi.html',
+            controller : 'poiCtrl as poiCtrl'
+        })
+        .otherwise({ redirectTo: '/' });
+}]);

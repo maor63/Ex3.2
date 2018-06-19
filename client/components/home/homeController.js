934c34349bea96f2910a06028587748820c1694e
sheinbey <gf>
2018-06-19 20:24:43 +0300

    rank and review works without username

--- client/components/home/homeController.js
+++ client/components/home/homeController.js
@@ -3,8 +3,12 @@ angular.module('citiesApp')
         self = this;
 
         var modal = document.getElementById('myModal');
+        var modalRank =document.getElementById('rankModal');
         var span = document.getElementsByClassName("close")[0];
-
+        var span2 = document.getElementsByClassName("close")[1];
+        $(':radio').change(function() {
+            self.rank=this.value;
+        });
 
         let serverUrl = 'http://localhost:8080/';
         self.sites = {};
@@ -13,6 +17,7 @@ angular.module('citiesApp')
         self.poiUrls={};
         self.reviews={};
         self.categories= {};
+
         loadCategoriesFromApi();
         $http.get("http://localhost:8080/sites/popular").then(function (answers) {
             let sites = answers.data;
@@ -94,7 +99,7 @@ angular.module('citiesApp')
             modal.style.display = "block";
         };
         self.openRankModal= function () {
-
+            modalRank.style.display = "block";
         };
         function getRandomSubarray(arr, size) {
             let shuffled = arr.slice(0), i = arr.length, min = i - size, temp, index;
@@ -115,6 +120,9 @@ angular.module('citiesApp')
 
        span.onclick = function() {
             modal.style.display = "none";
+        };
+        span2.onclick = function() {
+            modalRank.style.display = "none";
         };
 
        self.getImagesModal= function (siteID) {
@@ -158,6 +166,69 @@ angular.module('citiesApp')
                     alert('No Reviews on this site')
                 });
         };
+        self.submitRanking= function (review) {
+            if(!review|| !self.rankedStar)
+            {
+                alert('You must enter a literal review and rank the stars to submit');
+                return;
+            }
+            modalRank.style.display = "none";// close the ranking and review modal
+            var today = new Date();
+            var dd = today.getDate();
+            var mm = today.getMonth()+1; //January is 0!
+            var yyyy = today.getFullYear();
+
+            if(dd<10) {
+                dd = '0'+dd
+            }
+
+            if(mm<10) {
+                mm = '0'+mm
+            }
+            self.reviewObject=
+                {
+                    userName: "DNOT KNOW",
+                    siteID: self.poi[0].siteID,
+                    review: review,
+                    date: today,
+                }
+            self.rankObject=
+                {
+                    siteID: self.poi[0].siteID,
+                    rank: self.rankedStar,
+
+                }
+                self.submitTextReview();
+             self.submitRankReview();
+
+        };
+        self.submitTextReview= function () {
+            $http.post(serverUrl + "reg/review/", self.reviewObject)
+                .then(function (response) {
+                    //First function handles success
+                    self.content = response.data;
+                }, function (response) {
+                    console.log(response);
+                    alert('no success posting review')
+
+                });
+
+        };
+        self.submitRankReview= function () {
+            $http.post(serverUrl + "reg/rank/",self.rankObject)
+                .then(function (response) {
+                    //First function handles success
+                    self.content = response.data;
+                    alert('Review and Rank posted successfully');
+                }, function (response) {
+                    console.log(response);
+                    alert('no success posting rank')
+                });
+
+        };
+
+
+
 
         function loadCategoriesFromApi() {
             $http.get("http://localhost:8080/users/categories")
